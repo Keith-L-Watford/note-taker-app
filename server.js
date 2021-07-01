bcc6d1ca20ca1d194a7b9dd40e2c5bc8e8256be5
Keith Watford <keith.l.watford@gmail.com>
2021-07-01 18:59:15 -0400

    routed successfully to notes

--- server.js
+++ server.js
@@ -8,10 +8,24 @@ const PORT = process.env.PORT || 3000;
 const notes = [{},];
 
 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
 
+// routes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
 
+// the api stuff. GET and POST
+app.get('/api/notes/:notes', (req,res) => {
+
+});
+
+app.post('/api/notes/', (req, res) => {
+
+});
 
 
+// The Listener
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
