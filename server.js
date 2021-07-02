b30834fef5a7a67fcbc37a1d8faf06d0e8a7351a
Keith Watford <keith.l.watford@gmail.com>
2021-07-02 15:35:23 -0400

    i think i added the fs stuff correctly

--- server.js
+++ server.js
@@ -15,18 +15,7 @@ app.use(express.json());
 // recommended by Shane C.
 app.use(express.static(__dirname + '/public'));
 
-// const notes = [{
-//     "title": "test text",
-// },];
-
-
-// 
-
-
-
-
-
-// The api stuff with some readFile. GET and POST
+// The api stuff with readFile. GET and POST
 fs.readFile("db/db.json", "utf8", (err, dbjson) => {
     if (err) throw err;
     const notes = JSON.parse(dbjson)
@@ -36,6 +25,7 @@ fs.readFile("db/db.json", "utf8", (err, dbjson) => {
     app.post('/api/notes', (req, res) => {
         const aNewNote = req.body;
         notes.push(aNewNote)
+        addNewNoteToDB()
         console.log("testing to see if" + aNewNote.title + "was added");
     });
 
@@ -45,13 +35,10 @@ fs.readFile("db/db.json", "utf8", (err, dbjson) => {
     app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
     app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
-
-
+    addNewNoteToDB(() => {
+        fs.writeFile("db/db.json", JSON.stringify(notes), err)
+    });
 });
-
-
-
-
 
 // The Listener
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
