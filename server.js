acf60c2ae981f073545a1e828f5c81a3847d55f0
Keith Watford <keith.l.watford@gmail.com>
2021-07-02 18:28:56 -0400

    ran it back to a ersion that kind of worked.

--- server.js
+++ server.js
@@ -16,63 +16,49 @@ app.use(express.json());
 // app.use(express.static(__dirname + '/public'));
 app.use(express.static('public'));
 
-
-// =====================================================================
-
 // The api stuff with readFile. GET and POST
-// fs.readFile("db/db.json", "utf8", (err, data) => {});
-// if (err) throw err;
+fs.readFile("db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data)
 
-// Get/read the database
-app.get('/api/notes', (req, res) => {
-    let notes = JSON.parse(fs.readFileSync("./db/db.json","utf8"))
-    res.json(notes)
-});
+    app.get('/api/notes', (req, res) => res.json(notes));
 
-
-app.post('/api/notes', (req, res) => {
-    const aNewNote = req.body;
-    aNewNote.id = uniqid();
-
-    let notes = JSON.parse(fs.readFileSync("./db/db.json","utf8"))
-
-    notes.push(aNewNote)
- 
-    console.log("testing to see if " + aNewNote.title + " was added");
-});
-
-// ============
-// // stuff about unique ID numbers
-// app.get('/api/notes/:id', (req, res) => {
-
-//     res.json(notes);
-// });
-
-app.delete("/api/notes/:id", function (req, res) {
-    let noteId = req.params.id.toString();
-
-    let notes = JSON.parse(fs.readFileSync("./db/db.json","utf8"))
-    // notes.splice(noteId, 1);
-   
-
-    console.log("testing to see if " + noteId + " was deleted.");
-});
-
-// ==================================
-// to navigate between pages
-// moving these in here for now because why not
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
-
-// addNewNoteToDB()
-function addNewNoteToDB() {
-    fs.writeFile("db/db.json", JSON.stringify(notes), err => {
-        if (err) throw err;
-        return true;
+    app.post('/api/notes', (req, res) => {
+        const aNewNote = req.body;
+        notes.push(aNewNote)
+        addNewNoteToDB()
+        console.log("testing to see if " + aNewNote.title + " was added");
     });
-}
 
+    // ============
+    // stuff about unique ID numbers
+    app.get('/api/notes/:id', (req, res) => {
+      
+        res.json(notes);
+    });
 
+    app.delete("/api/notes/:id", function (req, res) {
+        const noteId = req.params.id;
+        notes.splice(noteId, 1);
+        addNewNoteToDB();
+        console.log("testing to see if " + noteId + " was deleted.");
+    });
+
+    // ==================================
+    // to navigate between pages
+    // moving these in here for now because why not
+    app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+    app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+
+    // addNewNoteToDB()
+    function addNewNoteToDB() {
+        fs.writeFile("db/db.json",JSON.stringify(notes),err => {
+            if (err) throw err;
+            return true;
+        });
+    }
+
+});
 
 
 // The Listener
