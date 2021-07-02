5177ffc8191dba6d48ed3c692460c60de1d2b2aa
Keith Watford <keith.l.watford@gmail.com>
2021-07-02 16:53:38 -0400

    commit what i currently have. just took a break

--- server.js
+++ server.js
@@ -43,9 +43,9 @@ fs.readFile("db/db.json", "utf8", (err, data) => {
     app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
     app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
-    addNewNoteToDB(() => {
-        fs.writeFile("db/db.json", JSON.stringify(notes), err)
-    });
+    // addNewNoteToDB() => {
+    //     fs.writeFile("db/db.json", JSON.stringify(notes), err)
+    // };
 });
 
 // The Listener
